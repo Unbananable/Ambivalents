d7260a75a30f93ee807fe5e02d2a62b891c86d16
Dimitri TRIGALOUX <dtrigalo@e3r7p21.42.fr>
2019-03-06 18:03:25 +0100

    added count room fill input

--- srcs/get_nb_rooms.c
+++ srcs/get_nb_rooms.c
@@ -6,12 +6,46 @@
 /*   By: dtrigalo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/06 16:51:30 by dtrigalo          #+#    #+#             */
-/*   Updated: 2019/03/06 16:59:17 by dtrigalo         ###   ########.fr       */
+/*   Updated: 2019/03/06 18:00:54 by dtrigalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+#include <unistd.h>
 
 /* Lecture, compter le nombre de rooms, et stocker tout stdin dans lem.input */
 
-static int	count_rooms(
+/* Ajouter realloc dans libft */
+
+int		count_room_and_fill_input(t_lem *lem)
+{
+	int		i;
+	int		count;
+	int		stop;
+	int		size;
+	char	buff[BUFF_SIZE];
+
+	i = -1;
+	stop = 0;
+	size = 0;
+	while ((size = read(0, buff, BUFF_SIZE)) > 0)
+	{
+		buff[size - 1] = '\0';
+		while (buff[++i])
+		{
+			if (buff[i] == '#')
+				while (buff[++i] != '\n')
+					;
+			else
+			{
+				count += (buff[i] == '\n' || !stop) ? 1 : 0;
+				stop = (buff[i] == '-') ? 1 : 0; //Ajouter gestion des salles avec un '-'
+			}
+		}
+		lem->input = ft_realloc(lem->input, sizeof(char) * (ft_strlen(lem->input) + size)); // Pas besoin de +1 je penses mais je laisse ça là en cas de segfault hein (y)
+		lem->input = ft_strncat(lem->input, buff, size); //strncat est plus lourd que strcat.. a-t-on vraiment besoin de la "sécurité" de strncat ? a retirer en cas de manque de perfs, et voir.
+	}
+	if (size < 0)
+		return (0);
+	return (count);
+}
