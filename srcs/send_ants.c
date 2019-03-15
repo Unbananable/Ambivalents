fdf8fa7934c1591c04f83cf256057e409b366447
Dimitri TRIGALOUX <dtrigalo@e3r8p20.42.fr>
2019-03-15 10:54:10 +0100

    up to you

--- srcs/send_ants.c
+++ srcs/send_ants.c
@@ -6,13 +6,11 @@
 /*   By: dtrigalo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 11:48:27 by dtrigalo          #+#    #+#             */
-/*   Updated: 2019/03/14 19:33:26 by dtrigalo         ###   ########.fr       */
+/*   Updated: 2019/03/15 10:53:52 by dtrigalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
-
-//ft_intlen a rajouter a la libft
 
 static void	make_w_list(t_lem *lem)
 {
@@ -33,9 +31,9 @@ static int	search_next_room(t_lem *lem, int i_room)
 {
 	int	j;
 
-	j = 0;
+	j = 0; 
 	while (++j < nb_rooms)
-		if (lem->links[i][j] == 1 && (lem->rooms[j].w || j == 1))
+		if (lem->links[i_room][j] == 1 && (lem->rooms[j].w || j == END) /* */)
 			return (j);
 	return (-1);
 }
@@ -50,7 +48,6 @@ static void	make_ants_move(t_lem *lem)
 	{
 		if (lem->rooms[i].is_full)
 		{
-/* Alors la suite est bien mignonne mais il va manquer le numero de la fourmi, donc je propose d associer une case a sa fourmi (et 0 en l absence de fourmi, vu que les fourmi ont un idx >= 1 (cf. t_room) */
 			if (!(next_room = search_next_room(lem, i)) == -1)
 				error(lem);
 			ft_char_realloc(lem->instr, ft_strlen(lem->instr)
@@ -82,6 +79,7 @@ void		send_ants(t_lem *lem)
 	make_w_list(lem);
 	while (ants_left)
 	{
+		make_ants_move(lem);
 		while (lem->w_list[++i] && ants_left
 				&& ants_left >= lem->rooms[lem->w_list[i]].w
 				- lem->rooms[lem->w_list[0]].w)
@@ -90,7 +88,7 @@ void		send_ants(t_lem *lem)
 			lem->rooms[lem->w_list[i]].is_full = 1;
 			ft_char_realloc(lem->instr, ft_strlen(lem->instr)
 					+ ft_intlen(lem->nb_ants - ants_left + 1)
-					+ ft_strlen(lem->rooms[lem->w_list[i]].id) + 3); // 3 pour '-',' ' et 'L'
+					+ ft_strlen(lem->rooms[lem->w_list[i]].id) + 3);
 			ft_strcat(lem->instr, "L");
 			ft_strcat(lem->instr, ft_itoa(lem->nb_ants - ants_left + 1));
 			ft_strcat(lem->instr, "-");
@@ -98,10 +96,14 @@ void		send_ants(t_lem *lem)
 			ft_strcat(lem->instr, " ");
 			ants_left--;
 		}
-		make_ants_move(lem);
 		lem->instr[ft_strlen(lem->instr) - 1] = '\n';
 		i = -1;
 	}
+	i = -1;
+	while (++i < lem->rooms[lem->w_list[0]])
+		make_ants_move(lem);
 	ft_putstr_fd(lem->instr, 1);
 	ft_strdel(lem->instr);
 }
+
+//put is full to 0 before start
