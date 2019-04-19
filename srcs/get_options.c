22adb636fd55ffcbe4d0d96ee9b06e4d414371ea
Anaelle LECLABART <anleclab@e3r7p21.42.fr>
2019-04-19 11:42:41 +0200

    added --help option and sorted test files

--- srcs/get_options.c
+++ srcs/get_options.c
@@ -6,11 +6,28 @@
 /*   By: anleclab <anleclab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/27 20:04:24 by anleclab          #+#    #+#             */
-/*   Updated: 2019/04/18 17:09:23 by anleclab         ###   ########.fr       */
+/*   Updated: 2019/04/19 11:07:14 by anleclab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+static void	print_help(void)
+{
+	ft_putstr("usage: ./lem-in [options] [< file]\n");
+	ft_putstr("available options:\n");
+	ft_putstr("\t--silent: do not print the regular output of the program\n");
+	ft_putstr("\t--show-paths: prints the name of the rooms in the paths the ");
+	ft_putstr("ants follow\n");
+	ft_putstr("\t--line-count: prints the number of lines of instructions ");
+	ft_putstr("necessary for all the ants to get to the end room\n");
+	ft_putstr("\t--ant-number: prints the number of ants\n");
+	ft_putstr("\t--ants-per-room: prints how many ants are sent in each path\n");
+	ft_putstr("\t--full-info: activates --line-count --ants-per-room ");
+	ft_putstr("--ant-number\n");
+	ft_putstr("\t--help: you are here\n");
+	exit(0);
+}
 
 static void	add_option(int *options, char c)
 {
@@ -31,6 +48,8 @@ static int	apply_option(char *av, int *options)
 		add_option(options, 'a');
 	else if (ft_strequ(av, "ants-per-room"))
 		add_option(options, 'r');
+	else if (ft_strequ(av, "help"))
+		print_help();
 	else if (ft_strequ(av, "full-info"))
 	{
 		add_option(options, 'l');
